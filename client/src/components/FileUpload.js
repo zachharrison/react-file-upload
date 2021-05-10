f6f1bdfd46a52a4651608d49df109ca9c0256810
Zach Harrison <zharrison@shaw.ca>
2021-05-10 14:58:49 -0700

    Can now upload files successfully

---
+++ client/src/components/FileUpload.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const FileUpload = () => {
+  const [file, setFile] = useState('');
+  const [fileName, setFileName] = useState('Choose file');
+  const [uploadedFile, setUploadedFile] = useState({});
+
+  const onChange = (e) => {
+    setFile(e.target.files[0]);
+    setFileName(e.target.files[0].name);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file', file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+
+      const { fileName, filePath } = res.data;
+
+      setUploadedFile({ fileName, filePath });
+    } catch (error) {
+      if (error.response.status === 500) {
+        console.log('There was a problem with the server');
+      } else {
+        console.log(error.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <div className='custom-file mb-4'>
+          <input
+            type='file'
+            className='custom-file-input'
+            id='customFile'
+            onChange={onChange}
+          />
+          <label htmlFor='customFile' className='custom-file-label'>
+            {fileName}
+          </label>
+        </div>
+        <input
+          type='submit'
+          value='Upload'
+          className='btn btn-primary btn-block mt-3'
+        />
+      </form>
+      {uploadedFile ? (
+        <div className='row mt-5'>
+          <div className='col-md-6 m-auto'>
+            <h3 className='text-center'>{uploadedFile.fileName}</h3>
+            <img
+              style={{ width: '100%' }}
+              src={uploadedFile.filePath}
+              alt='upload'
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
+  );
+};
+
+export default FileUpload;
